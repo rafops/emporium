4b267e8829583b0858655943407f93d523e18d7b
Rafael Moraes <contact@moraesrafael.com>
2017-07-09 22:41:46 -0400

    disable secret keys production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,6 @@ Rails.application.configure do
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
-  config.read_encrypted_secrets = true
+  ## Encrypted secrets only being used for development/testing at the moment
+  config.read_encrypted_secrets = false
 end
