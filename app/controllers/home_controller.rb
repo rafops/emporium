b6e74ef1e174137fbb16cd409c14de8b04e7ec9e
Rafael Moraes <contact@moraesrafael.com>
2017-07-30 18:19:42 -0400

    moving list photos to domain model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,24 @@
 class HomeController < ApplicationController
   def index
-    @thumbnails = Photo.thumbnail.joins(:original).order(created_at: :desc)
+    list_photos.on :success do |thumbnails|
+      respond_to do |format|
+        format.html { render :index, locals: { thumbnails: thumbnails } }
+      end
+    end
+
+    list_photos.on :not_found do
+      respond_to do |format|
+        format.json { render :index, locals: { thumbnails: [] } }
+      end
+    end
+
+    list_photos.call
   end
+
+  private
+
+    def list_photos
+      @list_photos ||= ListPhotos.new
+    end
+
 end
