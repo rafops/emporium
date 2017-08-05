bcd6a96fcc7ad485049a024ea6e13b55855fe41e
Rafael Moraes <contact@moraesrafael.com>
2017-08-05 19:57:51 -0400

    small fix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: :create
 
   def index
     list_events.on :success do |events|
